a5ffceecfe6cfbb7991104007dfa82b68590e7d6
Ian Wright <ian.wright@well.ox.ac.uk>
2013-02-21 12:26:59 +0000

    Updated for different input file with more fields

--- resources/sql/loadSamples.sql
+++ resources/sql/loadSamples.sql
@@ -1,15 +1,12 @@
-LOAD DATA LOCAL INFILE '/data/malariagen2/plasmodium/pf-crosses/meta/20121206/samples.txt' into table samples
+LOAD DATA LOCAL INFILE '/data/malariagen2/plasmodium/pf-crosses/meta/20121206/qcmeta.txt' into table pfx_samples
 
         FIELDS TERMINATED BY '\t' ENCLOSED BY '"' ESCAPED BY '"'
 
         LINES TERMINATED BY '\n'
 
         IGNORE 1 LINES 
-
         (
-
-      		`project_code`,`sample_id`,`ox_code`,`source_code`,`sra_accession`,`crossName`,`Run`,`Instrument_Model`,`Library_Name`,`Run_Read_Count`,`Run_Base_Count`
-
+      		`project_code`,`id`,`ox_code`,`source_code`,`sra_accession`,`cross`,`run`,`Instrument_Model`,`Library_Name`,`Run_Read_Count`,`Run_Base_Count`,`total`,`percent_mapped`,`percent_properly_paired`,`percent_singletons`,`percent_mate_other_chr`,`percent_duplicates`,`coverage`
         )
 
         ;
--- resources/sql/samples.sql
+++ resources/sql/samples.sql
@@ -1,14 +1,21 @@
-DROP TABLE IF EXISTS `samples`;
-CREATE TABLE `samples` (
+DROP TABLE IF EXISTS `pfx_samples`;
+CREATE TABLE `pfx_samples` (
 `project_code` varchar(64) NULL default NULL,
-`sample_id` varchar(64) NULL default NULL,
+`id` bigint(20) NULL default NULL,
 `ox_code` varchar(64) NULL default NULL,
 `source_code` varchar(64) NULL default NULL,
 `sra_accession` varchar(64) NULL default NULL,
-`crossName` varchar(64) NULL default NULL,
-`Run` varchar(64) NULL default NULL,
+`cross` varchar(64) NULL default NULL,
+`run` varchar(64) NULL default NULL,
 `Instrument_Model` varchar(64) NULL default NULL,
 `Library_Name` varchar(64) NULL default NULL,
 `Run_Read_Count` varchar(64) NULL default NULL,
-`Run_Base_Count` varchar(64) NULL default NULL
+`Run_Base_Count` varchar(64) NULL default NULL,
+`total` varchar(64) NULL default NULL,
+`percent_mapped` varchar(64) NULL default NULL,
+`percent_properly_paired` varchar(64) NULL default NULL,
+`percent_singletons` varchar(64) NULL default NULL,
+`percent_mate_other_chr` varchar(64) NULL default NULL,
+`percent_duplicates` varchar(64) NULL default NULL,
+`coverage` varchar(64) NULL default NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
